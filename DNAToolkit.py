79033c49bfe2d719600c98e68b87472f0cff3bb7
James Corines <jcorines@gmail.com>
2020-11-21 19:57:22 -0500

    added new files and GC content functions

--- DNAToolkit.py
+++ DNAToolkit.py
@@ -1,10 +1,13 @@
 # DNA Toolkit file
 import collections
+from structures import *
 
 Nucleotides = ["A", "C", "G", "T"]
-
+DNA_ReverseComplement = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
 
 # Check the sequence to make sure it is a DNA String
+
+
 def validateSeq(dna_seq):
     tmpseq = dna_seq.upper()
     for nuc in tmpseq:
@@ -21,3 +24,32 @@ def countNucFrequency(seq):
     return dict(collections.Counter(seq))
 
     # return dict(collections.Counter(seq))
+
+
+def transcription(seq):
+    """ DNA -> RNA transcription"""
+    return seq.replace("T", "U")
+
+
+def reverse_complement(seq):
+    """
+    swapping adenine with thyrmine and guanine with cytosine
+    Reversing generated string
+    """
+    return ''.join([DNA_ReverseComplement[nuc]for nuc in seq])[::-1]
+    # mapping = str.maketrans('ATCG', 'TAGC')
+    # return seq.translate(mapping)[::-1]
+
+
+def gc_count(seq):
+    """GC content is in a DNA/RNA sequce"""
+    return round((seq.count('C') + seq.count('G')) / len(seq) * 100)
+
+
+def gc_content_subsec(seq, k=20):
+    """ GC content in a DNA/RNA sub-sequence k. k=20 by deault"""
+    res = []
+    for i in range(0, len(seq) - k + 1, k):
+        subseq = seq[i: i + k]
+        res.append(gc_count(subseq))
+    return res
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 # main.py
 from DNAToolkit import *
+from utilities import colored
 import random
+
 
 rndDNAstr = ''.join([random.choice(Nucleotides)
                      for nuc in range(50)])
 DNAstr = validateSeq(rndDNAstr)
 print(countNucFrequency(rndDNAstr))
+print(transcription(DNAstr))
+
+tmpStr = "TEST"
+print(DNAstr)
+print(reverse_complement(DNAstr))
+
+print(f"{gc_count(DNAstr)}")
+print(f'{gc_content_subsec(DNAstr, k=5)}')
